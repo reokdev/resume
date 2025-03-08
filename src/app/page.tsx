fe599741a5174f646154a0481c1caa90276c4fe8
Rachael Oldham-Knott <reokwebdev@gmail.com>
2025-03-08 17:37:33 +0000

    added project, futher work on overall resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,8 +31,11 @@ export default function Page() {
               />
             </div>
             <BlurFade delay={BLUR_FADE_DELAY}>
-              <Avatar className="size-28 border">
-                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+              <Avatar className="h-28 w-28 border">
+                <AvatarImage 
+                  alt={DATA.name} 
+                  src={DATA.avatarUrl} 
+                />
                 <AvatarFallback>{DATA.initials}</AvatarFallback>
               </Avatar>
             </BlurFade>
